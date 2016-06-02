fbe7c2541b174372fcf00a51c6409417ee92fd4c
Dan Puttick <dputtick@users.noreply.github.com>
2016-06-02 09:39:01 -0400

    finished 1.5

--- set1.py
+++ set1.py
@@ -1,5 +1,7 @@
 import binascii
 import base64
+import itertools
+import operator
 
 
 def challenge1():
@@ -20,24 +22,56 @@ def challenge2():
     print(bytes(xorbytes).decode())
 
 
+def bytes_scorer(string_of_bytes):
+    target = string_of_bytes
+    spaces = target.count(b' ')
+    e = target.count(b'e')
+    t = target.count(b't')
+    a = target.count(b'a')
+    return spaces + e + t + a
+
+def best_string(string_of_bytes):
+    charlist = [num for num in bytes(range(128))]
+    best = (0, 0, 0)
+    for char in charlist:
+        xor_result = bytes(char ^ byte for byte in string_of_bytes)
+        scored_value = bytes_scorer(xor_result)
+        if scored_value > best[0]:
+            best = scored_value, xor_result, char
+    return best
+
+
 def challenge3():
     ch3str = b'1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
     ch3bytes = binascii.unhexlify(ch3str)
-    charlist = [num for num in bytes(range(128))]
-    un_xor_dict = {}
-    for char in charlist:
-        ch3result = bytes(char ^ byte for byte in ch3bytes)
-        un_xor_dict[char] = ch3result
-    ch3best = (0, 0, 0)
-    for key, value in un_xor_dict.items():
-        e = value.count(b'e')
-        t = value.count(b't')
-        a = value.count(b'a')
-        space = value.count(b' ')
-        comparer = sum([e, t, a, space])
-        if comparer > ch3best[0]:
-            ch3best = comparer, value, key
-    print(ch3best[1:])
+    print(best_string(ch3bytes))
 
-challenge3()
-    
+
+def challenge4():
+    with open('4.txt', 'r') as file:
+        best_overall = (0, 0, 0)
+        for line in file:
+            line = line.strip('\n')
+            line = binascii.unhexlify(line)
+            best_match = best_string(line)
+            if best_match[0] > best_overall[0]:
+                best_overall = best_match
+    print(best_overall)
+
+
+def repeating_key_xor(encrypt_string, key):
+    encrypt_string = binascii.a2b_qp(encrypt_string)
+    key = binascii.a2b_qp(key)
+    result = bytes(map(operator.xor, encrypt_string, itertools.cycle(key)))
+    print(binascii.hexlify(result))
+        
+
+def challenge5():
+    encrypt_string = ("Burning 'em, if you ain't quick and nimble\n"
+                "I go crazy when I hear a cymbal")
+    key = "ICE"
+    result = repeating_key_xor(encrypt_string, key)
+
+
+if __name__ == '__main__':
+    challenge5()
